f3ea487d7c539a8e5d7d509e72fdf4ee1719111a
jervisnd <nathan.jervis@phri.ca>
2015-08-11 15:00:48 -0400

    Initial Project checkin
    This includes lots of outdated code and things that don't belong, it is not at all cleaned up

---
+++ src/IronTuringTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronTuringTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
