fede4c77dc0a78d9f4a23bf8507235db1ebdf93f
mirhagk <mirhagk@gmail.com>
2015-08-25 13:15:21 -0400

    Create number literals

--- src/IronTuring/AST/LiteralNode.cs
+++ src/IronTuring/AST/LiteralNode.cs
@@ -9,6 +9,7 @@ namespace IronTuring.AST
     abstract class LiteralNode<T> : ExpressionNode
     {
         public virtual T Value { get; }
+        public override Type TypeOfExpression() => typeof(T);
     }
     class StringLiteralNode : LiteralNode<string>
     {
@@ -20,6 +21,32 @@ namespace IronTuring.AST
         public override void GenerateIL(ILGenerator il, SymbolTable st)
         {
             il.Emit(OpCodes.Ldstr, Value);
+        }
+    }
+    abstract class NumberLiteralNode<T>:LiteralNode<T>
+    {
+    }
+    static class NumberLiteralNode
+    {
+        public static ExpressionNode GetNumber(string number)
+        {
+            if (number.Contains("."))
+            {
+
+            }
+            return new IntegerLiteralNode(int.Parse(number));
+        }
+    }
+    class IntegerLiteralNode : NumberLiteralNode<int>
+    {
+        public override int Value { get; }
+        public IntegerLiteralNode(int value)
+        {
+            Value = value;
+        }
+        public override void GenerateIL(ILGenerator il, SymbolTable st)
+        {
+            il.Emit(OpCodes.Ldc_I4, Value);
         }
     }
 }
