700b8a3cb7f88000e9a271fcf98a09cc17f51f51
mirhagk <mirhagk@gmail.com>
2015-08-25 11:57:46 -0400

    created generator for put node

--- src/IronTuring/AST/PutNode.cs
+++ src/IronTuring/AST/PutNode.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 
 namespace IronTuring.AST
@@ -13,6 +14,17 @@ namespace IronTuring.AST
         {
             Items = items.ToList();
             NewLine = newLine;
+
+        }
+        public override void GenerateIL(ILGenerator il, SymbolTable st)
+        {
+            foreach(var item in Items)
+            {
+                item.GenerateIL(il, st);
+                il.Emit(OpCodes.Call, typeof(Console).GetMethod("Write", new Type[] { typeof(object) }));
+            }
+            if (NewLine)
+                il.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new Type[] { }));
         }
     }
 }
