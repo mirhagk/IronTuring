685f2110516974bb56833927a08bc03abf2000b5
mirhagk <mirhagk@gmail.com>
2015-08-25 13:15:07 -0400

    Make forloops work with blocks and start to reduce

--- src/IronTuring/AST/ForLoopNode.cs
+++ src/IronTuring/AST/ForLoopNode.cs
@@ -5,25 +5,37 @@ using System.Text;
 
 namespace IronTuring.AST
 {
-    class ForLoopNode : BlockStatementNode
+    class ForLoopNode : StatementNode
     {
         public abstract class LoopRange { }
-        public class LiteralRange : LoopRange {
-        public int Lower { get; }
-        public int Upper { get; }
+        public class LiteralRange : LoopRange
+        {
+            public int Lower { get; }
+            public int Upper { get; }
             public LiteralRange(int upper, int lower)
             {
                 Upper = upper;
                 Lower = lower;
             }
         }
+        public BlockNode Block { get; }
         public string Index { get; }
         public LoopRange Range { get; }
         public bool Decreasing { get; }
-        public ForLoopNode(IEnumerable<StatementNode> block, LoopRange range, bool decreasing) : base(block)
+        public int ByAmount { get; }
+        public ForLoopNode(BlockNode block, LoopRange range, bool decreasing, string index, int byAmount = 1)
         {
+            Block = block;
             Range = range;
             Decreasing = decreasing;
+            Index = index;
+            ByAmount = byAmount;
+        }
+        public override IEnumerable<Node> Reduce()
+        {
+            var statements = new List<StatementNode>();
+            //statements.Add(new );
+            return base.Reduce();
         }
     }
 }
