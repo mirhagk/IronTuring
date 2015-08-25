6944c62a88dd1f60cf729eb57f619f4d47670e93
mirhagk <mirhagk@gmail.com>
2015-08-25 12:03:08 -0400

    Give UnitNode ability to generate a program

--- src/IronTuring/AST/Unit.cs
+++ src/IronTuring/AST/Unit.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 
 namespace IronTuring.AST
@@ -16,12 +19,40 @@ namespace IronTuring.AST
     class ImportSectionNode : Node { }
     class UnitNode:Node
     {
-        public ProgramNode Program { get; }
+        public BlockNode Program { get; }
         public ImportSectionNode ImportSection { get; }
-        public UnitNode(ProgramNode program, ImportSectionNode importSection)
+        public UnitNode(BlockNode program, ImportSectionNode importSection)
         {
+
             Program = program;
             ImportSection = importSection;
+        }
+        public void CreateProgram(string moduleName)
+        {
+            AssemblyName name = new AssemblyName(Path.GetFileNameWithoutExtension(moduleName));
+            var asmb = AppDomain.CurrentDomain.DefineDynamicAssembly(name, AssemblyBuilderAccess.Save);
+
+            ModuleBuilder modb = asmb.DefineDynamicModule(moduleName);
+
+            var type = modb.DefineType("$program");
+
+            var symbolTable = new SymbolTable(type);
+
+
+            symbolTable.AddFunctionHeader("$main", MethodAttributes.Static, null, new FunctionDefinition.Argument[] { },"$program");
+
+            var il = symbolTable.functionTable["$main"].GetILGenerator();
+            
+            Program.GenerateIL(il, symbolTable);
+
+            il.Emit(OpCodes.Ret);
+
+            modb.CreateGlobalFunctions();
+            asmb.SetEntryPoint(symbolTable.functionTable["$main"].methodDefinition);
+
+            symbolTable.typeTable.types[0].typeBuilder.CreateType();
+
+            asmb.Save(moduleName);
         }
     }
 }
